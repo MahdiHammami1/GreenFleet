778a9d913d19f4cf6965c510d8a36dcba4c5221b
25533582 <63555173+25533582@users.noreply.github.com>
2025-04-11 16:43:30 +0100

    project architecture

---
+++ GreenBack/GreenBack/src/main/java/com/example/GreenBack/GreenBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreenBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenBackApplication.class, args);
+	}
+
+}
---
+++ GreenBack/GreenBack/src/test/java/com/example/GreenBack/GreenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreenBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
